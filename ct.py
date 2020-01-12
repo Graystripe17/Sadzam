9cadc4a9462c61dfc3454a5f5576e0bd016627ad
Winston Van <winston.van7@gmail.com>
2020-01-12 11:45:01 -0600

    CTR must pad with 0s, for transforms vary for large powers of 2. The returned FFT is longer than the original

--- ct.py
+++ ct.py
@@ -18,30 +18,36 @@ def is_power_of_two(n):
     else:
         return n & (n - 1) == 0
 
-def cooley_tukey_recursive(x):
-    N = len(x)
+def cooley_tukey_recursive(a):
+    x = a.copy()
     # Algorithm must be power of two size input
     # Padding by zero yields the same results
-    # while not is_power_of_two(N):
-    #    x.append(0)
-    if N <= 2:
-        x_odd = [x[0]]
-        x_even = [x[1]]
-    else:
-        x_odd = cooley_tukey_recursive(x[:N:2])
-        x_even = cooley_tukey_recursive(x[1:N:2])
-    half = N // 2
-    W = [cmath.exp(-2j * math.pi * k / N) for k, n in enumerate(range(half))]
-    weighted_even = [even * w for even, w in zip(x_even, W)]
-    begin = [x + w for x, w in zip(x_odd, weighted_even)]
-    end = [x - w for x, w in zip(x_odd, weighted_even)]
-    return begin + end
-    
-a = [0, 1, 3, 8, 0, 0, 0, 0] 
+    while not is_power_of_two(len(x)):
+        x.append(0)
+    def helper(x):
+        N = len(x)
+        if N <= 2:
+            x_odd = [x[0]]
+            x_even = [x[1]]
+        else:
+            x_odd = helper(x[:N:2])
+            x_even = helper(x[1:N:2])
+        half = N // 2
+        W = [cmath.exp(-2j * math.pi * k / N) for k in range(half)]
+        weighted_even = [even * w for even, w in zip(x_even, W)]
+        begin = [x + w for x, w in zip(x_odd, weighted_even)]
+        end = [x - w for x, w in zip(x_odd, weighted_even)]
+        return begin + end
+    return helper(x)
+
+a = [0, 1, 4, 3, 8, 3, 0, 0, 0] 
 F = fft(a)
 C = cooley_tukey_recursive(a)
 E = list(np.fft.fft(a))
-print(F)
-print(E)
+print("F", F)
+print("C", C)
+print("E", E)
+print(len(F), len(C), len(E))
 for i in range(len(a)):
-    assert(math.isclose(abs(E[i]), abs(C[i]), abs_tol=1e-8))
+    print(F[i], C[i], E[i])
+    assert(math.isclose(abs(F[i]), abs(E[i]), abs_tol=1e-8))
