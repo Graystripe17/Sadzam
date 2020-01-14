b5cd45c63300c19fd6a8b9035a01fe8dc32945fd
Winston Van <winston.van7@gmail.com>
2020-01-13 21:15:13 -0600

    Sped up padding, removed testing from ct.py

--- ct.py
+++ ct.py
@@ -19,13 +19,15 @@ def is_power_of_two(n):
     else:
         return n & (n - 1) == 0
 
+def get_next_power_of_two(n):
+    return pow(2, ceil(log(x)/log(2)))
 
 def cooley_tukey_recursive(a):
     x = a.copy()
     # Algorithm must be power of two size input
     # Padding by zero yields the same results
-    while not is_power_of_two(len(x)):
-        x.append(0)
+    if not is_power_of_two(len(x)):
+        x = x + [0] * (get_next_power_of_two(n) - len(x))
     def helper(x):
         N = len(x)
         if N <= 2:
@@ -41,23 +43,3 @@ def cooley_tukey_recursive(a):
         end = [x - w for x, w in zip(x_odd, weighted_even)]
         return begin + end
     return helper(x)
-
-a = [0, 1, 4, 3, 8, 3, 0, 2, 0, 4, 1, 1, 6, 8, 3, 1, 14, 23, 55] 
-F = fft(a)
-C = cooley_tukey_recursive(a)
-E = list(np.fft.fft(a))
-print("F", F)
-print("C", C)
-print("E", E)
-print(len(F), len(C), len(E))
-for i in range(len(a)):
-    print(F[i], C[i], E[i])
-    assert(math.isclose(abs(F[i]), abs(E[i]), abs_tol=1e-8))
-
-time = range(len(a))
-pyplot.plot(F, label='F')
-pyplot.plot(C, label='C')
-pyplot.plot(E, label='E')
-pyplot.plot(a, label='a')
-pyplot.legend()
-pyplot.show()
